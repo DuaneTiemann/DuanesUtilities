6b27807e7f1d53ed710378bf1fbf0d90edeb4e26
Duane Tiemann <duanetiemann@prodigy.net>
2018-11-02 09:19:41 -0400

    Change compile time

--- com/duane/utilities/CompileDate.java
+++ com/duane/utilities/CompileDate.java
@@ -1,2 +1,2 @@
 package com.duane.utilities;                                                           
-public class CompileDate{public static String getDate(){return "Mon 10/29/2018 23:00:21.92";}} 
+public class CompileDate{public static String getDate(){return "Fri 11/02/2018  7:22:34.01";}} 
